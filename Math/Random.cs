fe9c0883fd92868957a79d2637a55d33f6cc89b9
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-06-16 17:53:09 +0300

    ...

--- Math/Random.cs
+++ Math/Random.cs
@@ -4,14 +4,14 @@ namespace Common.Math
 {
     public static class Random
     {
-        public static double RandomDouble(double2 input)
+        public static double Double(double2 input)
         {
             return math.frac(math.sin(math.dot(input.xy, new double2(12.9898, 78.233))) * 43758.5453);
         }
 
-        public static double RandomDouble(double x, double y)
+        public static double Double(double x, double y)
         {
-            return RandomDouble(new double2(x, y));
+            return Double(new double2(x, y));
         }
     }
 }
