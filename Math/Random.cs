35f8fc261182b4b64862cf23ae3dd5be0c39d31f
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-06-18 20:57:58 +0300

    cool

--- Math/Random.cs
+++ Math/Random.cs
@@ -1,17 +1,17 @@
+using System;
 using Unity.Mathematics;
 
 namespace Common.Math
 {
     public static class Random
     {
-        public static double Double(double2 input)
-        {
-            return math.frac(math.sin(math.dot(input.xy, new double2(12.9898, 78.233))) * 43758.5453);
-        }
-
         public static double Double(double x, double y)
         {
-            return Double(new double2(x, y));
+            return math.frac(math.sin(math.dot(new double2(x, y), new double2(12.9898, 78.233))) * 43758.5453);
+        }
+        public static long Long(long x, long y)
+        {
+            return Convert.ToInt64(Double(x % 1000000000000000, y % 1000000000000000) * long.MaxValue);
         }
     }
 }
