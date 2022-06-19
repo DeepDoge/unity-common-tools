1c0060e1f315a9dccc4d1cfad7f0c9b852595b77
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-06-19 12:55:02 +0300

    ...

--- Math/Random.cs
+++ Math/Random.cs
@@ -1,5 +1,6 @@
 using System;
 using Unity.Mathematics;
+using UnityEngine;
 
 namespace Common.Math
 {
@@ -7,11 +8,13 @@ namespace Common.Math
     {
         public static double Double(double x, double y)
         {
-            return math.frac(math.sin(math.dot(new double2(x, y), new double2(12.9898, 78.233))) * 43758.5453);
+            return math.clamp(Mathf.PerlinNoise(Convert.ToSingle(x % 100000) * 100.1f, Convert.ToSingle(y % 100000) * 100.1f), 0.00000000000001d, .99999999999999d);
         }
+
         public static long Long(long x, long y)
         {
-            return Convert.ToInt64(Double(x % 1000000000000000, y % 1000000000000000) * long.MaxValue);
+            var result = Double(x % 1000000000000000, y % 1000000000000000);
+            return Convert.ToInt64(result * long.MaxValue);
         }
     }
 }
